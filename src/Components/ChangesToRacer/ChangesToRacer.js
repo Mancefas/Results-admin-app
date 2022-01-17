45c1956f83ea18a99b51e26914a330253417dfd2
Mancefas <mantvydasliobe@gmail.com>
2022-01-17 21:04:22 +0200

    added : success,error mssg for changing racer time

--- src/Components/ChangesToRacer/ChangesToRacer.js
+++ src/Components/ChangesToRacer/ChangesToRacer.js
@@ -1,6 +1,13 @@
 import React, { useState, useEffect, useContext } from "react";
 
-import { Container, TextField, Box, Button } from "@mui/material";
+import {
+  Container,
+  TextField,
+  Box,
+  Button,
+  Alert,
+  LinearProgress,
+} from "@mui/material";
 
 import config from "../../config.json";
 import AuthContext from "../../store/auth-context";
@@ -13,9 +20,45 @@ const ChangesToRacer = () => {
   const [newTime, setNewTime] = useState();
   const [dataOfAllResults, setDataOfAllResults] = useState();
 
+  const [loadingMessage, setLoadingMessage] = useState(false);
+  const [successMessage, setSuccessMessage] = useState(false);
+  const [errorMessage, setErrorMessage] = useState(false);
+
   const racerNrHandler = (number) => {
     setRacerNr(number);
   };
+
+  // functions to show only one message at the time
+  const loadingMessageHandler = () => {
+    setLoadingMessage(true);
+    setSuccessMessage(false);
+    setErrorMessage(false);
+  };
+  const sucessMessageHandler = () => {
+    setLoadingMessage(false);
+    setSuccessMessage(true);
+    setErrorMessage(false);
+  };
+  const errorMessageHandler = (message) => {
+    setLoadingMessage(false);
+    setSuccessMessage(false);
+    setErrorMessage(message);
+  };
+
+  //show success or error message only for few seconds
+  useEffect(() => {
+    setTimeout(() => {
+      if (successMessage) {
+        setSuccessMessage(false);
+      }
+      if (errorMessage) {
+        setErrorMessage(false);
+      }
+    }, 2500);
+    return () => {
+      clearTimeout();
+    };
+  }, [successMessage, errorMessage]);
 
   // Get data of all racers
   const fetchindData = async () => {
@@ -33,10 +76,15 @@ const ChangesToRacer = () => {
         });
       }
       setDataOfAllResults(dataToArrayOfObjects);
+      if (response.ok) {
+      } else {
+        throw new Error(response.message);
+      }
     } catch (error) {
-      console.log(error);
+      errorMessageHandler(error.message);
     }
   };
+
   useEffect(() => {
     fetchindData();
   }, []);
@@ -48,17 +96,23 @@ const ChangesToRacer = () => {
       (number) => number.dalyvis === racerNR
     );
     const firebaseIdOfRacer = racerForChanges[0].id;
+    loadingMessageHandler();
 
     try {
-      await fetch(
+      const response = await fetch(
         `${config.API_URL_FOR_CHANGES}${firebaseIdOfRacer}.json?auth=${context.token}`,
         {
           method: "PATCH",
           body: JSON.stringify({ finisoLaikas: `${changedTime}` }),
         }
       );
+      if (response.ok) {
+        sucessMessageHandler();
+      } else {
+        throw new Error(response.statusText);
+      }
     } catch (error) {
-      console.log(error);
+      errorMessageHandler(error.message);
     }
   };
 
@@ -95,6 +149,27 @@ const ChangesToRacer = () => {
         height: "fit-content",
       }}
     >
+      {loadingMessage && (
+        <LinearProgress sx={{ width: "50%", margin: "auto" }} />
+      )}
+      {successMessage && (
+        <Alert
+          severity="success"
+          variant="outlined"
+          sx={{ width: "fit-content", margin: "auto" }}
+        >
+          Laikas pakeistas!
+        </Alert>
+      )}
+      {errorMessage && (
+        <Alert
+          severity="error"
+          variant="outlined"
+          sx={{ width: "fit-content", margin: "auto" }}
+        >
+          Klaida! ({errorMessage})
+        </Alert>
+      )}
       <form onSubmit={submitedHandler}>
         <Box p={1}>
           <SelectButton
